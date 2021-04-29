87a132ec606ba2a8f7b97a4b377b53e531e9048f
Lviana21 <lucianoviana12@gmail.com>
2021-04-29 08:20:01 -0300

    Implementado método equals hashCode

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -10,7 +10,7 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		/*1° PASSAR OS DADOS "ENTRADA DE DADOS"*/
-		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		/*String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 		String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
 		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento?");
 		String identidade = JOptionPane.showInputDialog("Qual a identidade do aluno?");
@@ -23,15 +23,15 @@ public class PrimeiraClasseJava {
 		String nota1 = JOptionPane.showInputDialog("Nota1?");
 		String nota2 = JOptionPane.showInputDialog("Nota2?");
 		String nota3 = JOptionPane.showInputDialog("Nota3?");
-		String nota4 = JOptionPane.showInputDialog("Nota4?");
+		String nota4 = JOptionPane.showInputDialog("Nota4?");*/
 		
 		/*2° INSTANCIAR OS OBJETOS*/
 		/* new Aluno() é uma instancia (Criação de Objeto)*/
-		/* aluno1 é uma referencia para o obejto aluno*/
+		/* aluno1 é uma referencia para o obejto aluno
 		Aluno aluno1 = new Aluno(); /*Aqui será João "irá receber um valor qualquer"*/
 		
 		/*3° PASSAR OS VALORES QUE VEIO DA ENTRADA DE DADOS PARA O OBJETO*/
-		aluno1.setNome(nome);
+		/*aluno1.setNome(nome);
 		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(dataNascimento);
 		aluno1.setRegistroGeral(identidade);
@@ -44,7 +44,7 @@ public class PrimeiraClasseJava {
 		aluno1.setNota1(Double.valueOf(nota1));
 		aluno1.setNota2(Double.valueOf(nota2));
 		aluno1.setNota3(Double.valueOf(nota3));
-		aluno1.setNota4(Double.valueOf(nota4));
+		aluno1.setNota4(Double.valueOf(nota4));*/
 		
 		/*System.out.println("Nome: " + aluno1.getNome() + "\n" +
 		"Idade: " + aluno1.getIdade() + "\n" + "Data Nascimento: " + aluno1.getDataNascimento() + "\n" + 
@@ -59,15 +59,28 @@ public class PrimeiraClasseJava {
 		
 		/*================================================================================================================*/
 		/*4° PROCESSAR E TER O RESULTADO FINAL*/
-		System.out.println(aluno1.toString()); /*Descrição do objeto na memória*/
+		/*System.out.println(aluno1.toString()); Descrição do objeto na memória
 		System.out.println("Média do aluno = " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());*/
 		
 		
+		/*Equals e hashcode (Diferenciar e comparar objetos)*/
 		
-		Aluno aluno3 = new Aluno("Luciano");
 		
-		Aluno aluno4 = new Aluno("Luciano", 30);
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Luciano");
+		aluno1.setNumeroCpf("1234");
+		
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Luciano");
+		aluno2.setNumeroCpf("1234");
+		/*Parar comparar objetos se são iguais tem que sobrescrever o método "equals hashCode"*/
+		if(aluno1.equals(aluno2)) {
+			System.out.println("Alunos são iguais");
+		}else {
+			System.out.println("Alunos não são iguais");
+		}
+		
 		
 		
 }
