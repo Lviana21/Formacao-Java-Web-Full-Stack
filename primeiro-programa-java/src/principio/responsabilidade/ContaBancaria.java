073f84239dd9d67feffab14183beba560aa027b0
Lviana21 <lucianoviana12@gmail.com>
2021-05-17 07:20:44 -0300

    Adicionado Package principio de responsabilidade "Class conta bancária"

---
+++ primeiro-programa-java/src/principio/responsabilidade/ContaBancaria.java
@@ -0,0 +1,43 @@
+package princio.responsabilidade;
+
+public class ContaBancaria {
+
+	/* Metodos Soma, Diminui, Deposita, Saque */
+	private String Descricao;
+	private double saldo = 8000;
+
+	public void soma100Reias() {
+		saldo += 100;
+	}
+
+	public void diminui100Reias() {
+		saldo -= 100;
+	}
+
+	public void sacarDinhiero(double saque) {
+		saldo -= saque;
+
+	}
+
+	public void depositaDinheiro(double deposita) {
+		saldo += deposita;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return Descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "ContaBancaria [Descricao=" + Descricao + ", saldo=" + saldo + "]";
+	}
+
+}
--- primeiro-programa-java/src/principio/responsabilidade/TesteConta.java
+++ primeiro-programa-java/src/principio/responsabilidade/TesteConta.java
@@ -3,25 +3,22 @@ package princio.responsabilidade;
 public class TesteConta {
 
 	public static void main(String[] args) {
-		
+
 		ContaBancaria bancaria = new ContaBancaria();
-		bancaria.setDescricao("Conta bancaria do Luciano");
-		
+		bancaria.setDescricao("Conta bancária do Luciano");
+
+		bancaria.diminui100Reias();
 		System.out.println(bancaria);
-		
-		bancaria.diminui100Reais();
-		bancaria.diminui100Reais();
+
+		bancaria.soma100Reias();
 		System.out.println(bancaria);
-		
-		bancaria.sacarDinheiro(400);
+
+		bancaria.sacarDinhiero(500);
 		System.out.println(bancaria);
-		
-		bancaria.depositoDinheiro(1000);
+
+		bancaria.depositaDinheiro(500);
 		System.out.println(bancaria);
-		
-		bancaria.soma100Reais();
-		System.out.println(bancaria);
-		
+
 	}
 
 }
