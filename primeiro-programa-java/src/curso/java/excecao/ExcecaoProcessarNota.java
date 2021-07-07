0ea5bbe103fb82dabd62759f2f0ef04047df5818
Luciano Viana <lucianoviana12@gmail.com>
2021-07-07 06:28:15 -0300

    adicionado Class "ExcecaoProcessarNota" e adicionado método "lerArquivo" para utilizar excecao customizada.

---
+++ primeiro-programa-java/src/curso/java/excecao/ExcecaoProcessarNota.java
@@ -0,0 +1,8 @@
+package curso.java.excecao;
+
+public class ExcecaoProcessarNota extends Exception {
+
+	public ExcecaoProcessarNota(String erro) {
+		super("Erro no processamento do arquivo ao processas as notas do aluno!! " + erro);
+	}
+}
