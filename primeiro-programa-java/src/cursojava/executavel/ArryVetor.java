02d3952417511b56f596b4daa0577ebafcbb239d
Luciano Viana <lucianoviana12@gmail.com>
2021-07-09 08:15:09 -0300

    adicionado class "ArrayVetor"

---
+++ primeiro-programa-java/src/cursojava/executavel/ArryVetor.java
@@ -0,0 +1,25 @@
+package cursojava.executavel;
+
+public class ArryVetor {
+	
+	//Usando para executar códigos
+	public static void main(String[] args) {
+		
+		/*Array pode ser de todos os tipos de dados e objetivos também*/
+		/*Array sempre deve ter a quantidade de posições definidas*/
+		
+		double[] notas = new double[5];
+		
+		/*Atribuir valor nas posições do arrays*/
+		
+		notas[0] = 7.4;
+		notas[1] = 8.5;
+		notas[2] = 5.1;
+		notas[3] = 9.9;
+		notas[4] = 10;
+		
+		System.out.println(notas);
+		
+	}
+
+}
