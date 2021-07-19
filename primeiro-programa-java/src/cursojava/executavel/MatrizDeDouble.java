fb360fe1b830e6ac6143a921e274f34a8fff8b76
Luciano Viana <lucianoviana12@gmail.com>
2021-07-19 08:23:57 -0300

    Matriz de valores de ponto flutuante.

---
+++ primeiro-programa-java/src/cursojava/executavel/MatrizDeDouble.java
@@ -0,0 +1,26 @@
+package cursojava.executavel;
+
+public class MatrizDeDouble {
+
+	public static void main(String[] args) {
+
+		double notas[][] = new double[2][3];
+
+		// Matriz 01
+		notas[0][0] = 7.8;
+		notas[0][1] = 7.7;
+		notas[0][2] = 9.5;
+
+		// Matriz 02
+		notas[1][0] = 8.0;
+		notas[1][1] = 9.2;
+		notas[1][2] = 8.4;
+
+		for (int posLinha = 0; posLinha < notas.length; posLinha++) {
+			for (int posColuna = 0; posColuna < notas[posLinha].length; posColuna++) {
+				System.out.println("Valor Matriz: " + notas[posLinha][posColuna]);
+			}
+			System.out.println("-------------------------------------------------");
+		}
+	}
+}
