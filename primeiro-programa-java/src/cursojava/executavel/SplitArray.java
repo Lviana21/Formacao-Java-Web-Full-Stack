7c253e3cea44eed71394f2c896c4f88974916d50
Luciano Viana <lucianoviana12@gmail.com>
2021-07-20 06:24:35 -0300

    Utilizando o método Split.

---
+++ primeiro-programa-java/src/cursojava/executavel/SplitArray.java
@@ -0,0 +1,26 @@
+package cursojava.executavel;
+
+public class SplitArray {
+
+	public static void main(String[] args) {
+
+		String texto = "Luciano,Curso Java,80,90,76,82";
+
+		/*
+		 * Split é utilizado para dividir uma String em pequenos pedaços, o
+		 * método retorno um Array de Strings.
+		 */
+		String valorArray[] = texto.split(",");
+
+		for (int pos = 0; pos < valorArray.length; pos++) {
+			if (pos == 0) {
+				System.out.println("Nome Aluno: " + valorArray[pos]);
+			} else if (pos == 1) {
+				System.out.println("Curso: " + valorArray[pos]);
+			} else {
+				System.out.println("Nota " + (pos - 1) + " : " + valorArray[pos]);
+			}
+		}
+	}
+
+}
