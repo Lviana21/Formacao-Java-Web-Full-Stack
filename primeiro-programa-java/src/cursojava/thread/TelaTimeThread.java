b97526590cb8dc76fb93e108fa8a179ef6c76316
Luciano Viana <lucianoviana12@gmail.com>
2021-07-29 08:31:25 -0300

    ajustes de comentários em alguns blocos de códigos

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -64,7 +64,6 @@ public class TelaTimeThread extends JDialog {
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
@@ -78,7 +77,7 @@ public class TelaTimeThread extends JDialog {
 	public TelaTimeThread() { /* Executa o que tiver dentro no momento da abertura ou execução */
 		// Bloco de configurações iniciais
 		setTitle("Minha tela de time com Thread"); // Titulo
-		setSize(new Dimension(240, 240)); // Dimensão largura e altura
+		setSize(new Dimension(240, 240)); // Dimensão largura e altura da tela
 		setLocationRelativeTo(null); // Centralizando a tela
 		setResizable(false); // Travar a tela para não mudar o tamanho manual
 		/* Primeira parte concluída */
@@ -153,7 +152,7 @@ public class TelaTimeThread extends JDialog {
 		jButton2.setEnabled(false);
 
 		add(jPanel, BorderLayout.WEST);
-		// Sempre será o último comando
+		// Sempre será o último comando a ser executado
 		setVisible(true)/* Torna a tela visil para o usuário */;
 	}
 
