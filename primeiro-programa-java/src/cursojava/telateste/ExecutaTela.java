a22788a6a49ee0e4fab609b3ce874c2a6b6939d8
Luciano Viana <lucianoviana12@gmail.com>
2021-07-30 06:41:27 -0300

    Adicionado PacKage "telateste" com Class "ExecutaTela e TelaDeTempo".

---
+++ primeiro-programa-java/src/cursojava/telateste/ExecutaTela.java
@@ -0,0 +1,10 @@
+package cursojava.telateste;
+
+public class ExecutaTela {
+	
+	public static void main(String[] args) {
+		
+		TelaDeTempo telaDeTempo = new TelaDeTempo();
+	}
+
+}
---
+++ primeiro-programa-java/src/cursojava/telateste/TelaDeTempo.java
@@ -0,0 +1,144 @@
+package cursojava.telateste;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class TelaDeTempo extends JDialog {
+
+	private JPanel jPanel = new JPanel(new GridBagLayout());
+
+	private JLabel descricaoHora = new JLabel("Data / Hora Atual 01");
+	private JTextField mostraTempo = new JTextField();
+
+	private JLabel descricaoHora2 = new JLabel("Data / Hora Atual 02");
+	private JTextField mostraTempo2 = new JTextField();
+
+	private JButton jButton = new JButton("INICIAR");
+	private JButton jButton2 = new JButton("PARAR");
+
+	private Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			while (true) {
+				mostraTempo
+						.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
+
+	private Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+			while (true) {
+				mostraTempo2
+						.setText(new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
+
+	private Thread thread1Time;
+	private Thread thread2Time;
+
+	public TelaDeTempo() {
+
+		setTitle("Tela de Tempo");
+		setSize(new Dimension(240, 240));
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 2;
+		gridBagConstraints.insets = new Insets(2, 10, 8, 6);
+		gridBagConstraints.anchor = GridBagConstraints.WEST;
+
+		descricaoHora.setPreferredSize(new Dimension(200, 25));
+		jPanel.add(descricaoHora, gridBagConstraints);
+
+		mostraTempo.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		mostraTempo.setEditable(false);
+		jPanel.add(mostraTempo, gridBagConstraints);
+
+		descricaoHora2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(descricaoHora2, gridBagConstraints);
+
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
+		gridBagConstraints.gridy++;
+		mostraTempo2.setEditable(false);
+		jPanel.add(mostraTempo2, gridBagConstraints);
+
+		gridBagConstraints.gridwidth = 1;
+
+		jButton.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridy++;
+		jPanel.add(jButton, gridBagConstraints);
+
+		jButton2.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridx++;
+		jPanel.add(jButton2, gridBagConstraints);
+
+		jButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+
+				thread2Time = new Thread(thread2);
+				thread2Time.start();
+
+				jButton.setEnabled(false);
+				jButton2.setEnabled(true);
+			}
+		});
+
+		jButton2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				thread1Time.stop();
+				thread2Time.stop();
+
+				jButton.setEnabled(true);
+				jButton2.setEnabled(false);
+
+			}
+		});
+
+		add(jPanel, BorderLayout.WEST);
+		setVisible(true);
+	}
+
+}
