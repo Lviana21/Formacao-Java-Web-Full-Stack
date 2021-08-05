3a3453330315d56d62a009e0c831f753d228faa0
Luciano Viana <lucianoviana12@gmail.com>
2021-08-05 07:03:48 -0300

    Adicionado Class "DatasDeVencimento"

---
+++ primeiro-programa-java/src/modulo_datas/DatasDeVencimento.java
@@ -0,0 +1,25 @@
+package modulo_datas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatasDeVencimento {
+
+	public static void main(String[] args) throws ParseException {
+
+		Date dataInicial = new SimpleDateFormat("dd/MM/yyyy").parse("05/08/2021");// Data inicial
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dataInicial);
+		
+		for (int parcela = 1; parcela <= 12; parcela++) {
+			calendar.add(Calendar.MONTH, 1);
+
+			System.out.println("Parcela número: " + parcela + " vencimento é em: "
+					+ new SimpleDateFormat("dd/MM/yyyy").format(calendar.getTime()));
+		}
+	}
+
+}
