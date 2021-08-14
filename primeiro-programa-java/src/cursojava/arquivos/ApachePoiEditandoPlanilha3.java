48ef382086d7f886f70df4554b260f092cdbc348
Luciano Viana <77491260+Lviana21@users.noreply.github.com>
2021-08-14 09:07:54 -0300

    Adicionado Classe "ApachePoiEditandoPlanilha3"

---
+++ primeiro-programa-java/src/cursojava/arquivos/ApachePoiEditandoPlanilha3.java
@@ -0,0 +1,47 @@
+package cursojava.arquivos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ApachePoiEditandoPlanilha3 {
+
+	public static void main(String[] args) throws Exception {
+
+		// Pegar arquivo onde será alterado
+		File file = new File(
+				"C:\\Users\\Luciano Viana\\git\\Formacao-Java-Web-Full-Stack\\primeiro-programa-java\\src\\cursojava\\arquivos\\arquivo_excel.xls");
+
+		// Entrada de dados
+		FileInputStream entrada = new FileInputStream(file);
+
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(entrada); // Prepara a entrada do arquivo xsl do excel
+		HSSFSheet planilha = hssfWorkbook.getSheetAt(0); // Pegando a planilha
+
+		Iterator<Row> linhaIterator = planilha.iterator();// Percorrer linhas
+
+		while (linhaIterator.hasNext()) {// Enquanto tiver linha
+			Row linha = linhaIterator.next();// Dados da pessoa na linha
+
+			//Alterando dados da planilha
+			String valorCelula = linha.getCell(0).getStringCellValue();
+			linha.getCell(0).setCellValue(valorCelula + " * Valor gravado na aula *");
+			
+		}
+		entrada.close();
+
+		FileOutputStream saida = new FileOutputStream(file);// Saida de dados para o mesmo arquivo excel
+		hssfWorkbook.write(saida);// Escrita para a saida
+		saida.flush();
+		saida.close();
+
+		System.out.println("Planilha atualizada");
+	}
+}
