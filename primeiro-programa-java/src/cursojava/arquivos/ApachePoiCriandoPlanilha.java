eb69584975b07c1db8fda100ee53b7750d9c9406
Luciano Viana <77491260+Lviana21@users.noreply.github.com>
2021-08-14 08:52:03 -0300

    Modificado a descrição de Classes ApachePoi para "Criação, Leitura e edição de panilha".

--- primeiro-programa-java/src/cursojava/arquivos/ApachePoiCriandoPlanilha.java
+++ primeiro-programa-java/src/cursojava/arquivos/ApachePoiCriandoPlanilha.java
@@ -1,9 +1,11 @@
 package cursojava.arquivos;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -11,13 +13,11 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
-
-
-public class ApachePoi {
-
+public class ApachePoiCriandoPlanilha {
+	
 	public static void main(String[] args) throws Exception {
 		
-		File file = new File("C:\\Users\\Luciano Viana\\git\\Formacao-Java-Web-Full-Stack\\primeiro-programa-java\\src\\cursojava\\arquivos\\arquivo_excel.xls");
+File file = new File("C:\\Users\\Luciano Viana\\git\\Formacao-Java-Web-Full-Stack\\primeiro-programa-java\\src\\cursojava\\arquivos\\arquivo_excel.xls");
 		
 		if(!file.exists()) {
 			file.createNewFile();
--- primeiro-programa-java/src/cursojava/arquivos/ApachePoiEditandoPlanilha.java
+++ primeiro-programa-java/src/cursojava/arquivos/ApachePoiEditandoPlanilha.java
@@ -10,18 +10,20 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 
-public class ApachePoiEditando {
+public class ApachePoiEditandoPlanilha {
 	
 	public static void main(String[] args) throws Exception {
 		
+		//Pegar arquivo onde será alterado 
 		File file = new File("C:\\Users\\Luciano Viana\\git\\Formacao-Java-Web-Full-Stack\\primeiro-programa-java\\src\\cursojava\\arquivos\\arquivo_excel.xls");
 		
+		//Entrada de dados
 		FileInputStream entrada = new FileInputStream(file);
 		
 		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(entrada); //Prepara a entrada do arquivo xsl do excel
 		HSSFSheet planilha = hssfWorkbook.getSheetAt(0); //Pegando a planilha
 		
-		Iterator<Row> linhaIterator = planilha.iterator();
+		Iterator<Row> linhaIterator = planilha.iterator();//Percorrer linhas
 		
 		while (linhaIterator.hasNext()) {//Enquanto tiver linha
 			Row linha = linhaIterator.next();//Dados da pessoa na linha
@@ -34,8 +36,8 @@ public class ApachePoiEditando {
 		
 		entrada.close();
 		
-		FileOutputStream saida = new FileOutputStream(file);
-		hssfWorkbook.write(saida);
+		FileOutputStream saida = new FileOutputStream(file);//Saida de dados para o mesmo arquivo excel
+		hssfWorkbook.write(saida);//Escrita para a saida
 		saida.flush();
 		saida.close();
 		
