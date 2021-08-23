32b1fe56af8f0eaf13035d78f543e9f6bb7c7d7f
Luciano Viana <77491260+Lviana21@users.noreply.github.com>
2021-08-23 08:27:14 -0300

    Estudos conceitos e mentalidade de um Dev

--- primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
+++ primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
@@ -3,5 +3,6 @@ package cursojava.statusdev;
 public class AndamentoDoCurso {
 	
 	// 18-08-21: Instalação e configuração banco de dados PostgreSQL
+	// 23-08-21: Conceitos e mentalidade de um Dev
 
 }
