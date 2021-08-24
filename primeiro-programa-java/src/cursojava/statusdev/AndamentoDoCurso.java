1d301843d4cb5adea773c3c33dbad2b8cb904d29
Luciano Viana <77491260+Lviana21@users.noreply.github.com>
2021-08-24 08:40:48 -0300

    Visto Modelo Mental de Estudos

--- primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
+++ primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
@@ -4,5 +4,6 @@ public class AndamentoDoCurso {
 	
 	// 18-08-21: Instalação e configuração banco de dados PostgreSQL
 	// 23-08-21: Conceitos e mentalidade de um Dev
+	// 24-08-21: Modelo Mental de Estudos 
 
 }
