129573adf13609daadc90ff9d1726806b80696f9
Luciano Viana <77491260+Lviana21@users.noreply.github.com>
2021-08-25 07:42:59 -0300

    Teste de Commit

--- primeiro-programa-java/src/curso/java/excecao/ExcecaoProcessarNota.java
+++ primeiro-programa-java/src/curso/java/excecao/ExcecaoProcessarNota.java
@@ -4,5 +4,6 @@ public class ExcecaoProcessarNota extends Exception {
 
 	public ExcecaoProcessarNota(String erro) {
 		super("Erro no processamento do arquivo ao processas as notas do aluno!! " + erro);
+		
 	}
 }
