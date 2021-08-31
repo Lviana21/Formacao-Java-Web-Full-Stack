0e10afff78e088d356cefe62cde79e8d723fc1a1
Luciano Viana <77491260+Lviana21@users.noreply.github.com>
2021-08-31 08:59:20 -0300

    Pesquisando solução para erro de autocomplete no arquivo index.html - Não está encontrando o Document em javascript

--- primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
+++ primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
@@ -5,6 +5,6 @@ public class AndamentoDoCurso {
 	// 18-08-21: Instalação e configuração banco de dados PostgreSQL
 	// 23-08-21: Conceitos e mentalidade de um Dev
 	// 24-08-21: Modelo Mental de Estudos 
-	// 31-08-21: Pesquisando solução para erro de autocomplete no arquivo index.html - Não está encontrando o Document em javascript
+	// 31-08-21: Pesquisando solução para erro de autocomplete no arquivo index.html - IDE não está encontrando o Document em javascript
 
 }
