77cc81d30bf5bb49a35d904e17b3d96c0ba60142
Luciano Viana <77491260+Lviana21@users.noreply.github.com>
2021-09-01 06:46:11 -0300

    Downgrade da versão do Eclipse IDE de 2021 para 2019 para resolver problema de autocomplete JavaScript

--- primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
+++ primeiro-programa-java/src/cursojava/statusdev/AndamentoDoCurso.java
@@ -6,5 +6,5 @@ public class AndamentoDoCurso {
 	// 23-08-21: Conceitos e mentalidade de um Dev
 	// 24-08-21: Modelo Mental de Estudos 
 	// 31-08-21: Pesquisando solução para erro de autocomplete no arquivo index.html - IDE não está encontrando o Document em javascript
-
+	// 01-09-21: Downgrade da versão do Eclipse IDE para 2019 para resolver problema de autocomplete JavaScript
 }
